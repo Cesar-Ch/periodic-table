3e0d0a63518610382bbe859d7f7a194c9943d1eb
Cesar-Ch <chafloque1530@gmail.com>
2023-09-02 16:57:16 -0500

    🐞 bug resolved

--- src/components/Box.jsx
+++ src/components/Box.jsx
@@ -1,6 +1,6 @@
 const Box = ({ element, name, group, number, selectedGroup }) => {
     return (
-        <div className={`element ${group} ${selectedGroup === group ? '' : 'opacity-60'} ${selectedGroup === 'all' ? 'opacity-100' : ''}  w-[75px] h-[75px] relative rounded-md mr-1 mb-1 p-1 text-center cursor-pointer`} style={{ border: `2px solid #3f4a53`, color: `var(--${group})`, backgroundColor: `var(--${group})`, boxShadow: `1px 2.5px 1px #555` }}
+        <div className={`element ${group} ${(selectedGroup === 'all') || (selectedGroup === group) ? 'opacity-100' : 'opacity-60'}   w-[75px] h-[75px] relative rounded-md mr-1 mb-1 p-1 text-center cursor-pointer`} style={{ border: `2px solid #3f4a53`, color: `var(--${group})`, backgroundColor: `var(--${group})`, boxShadow: `1px 2.5px 1px #555` }}
             onClick={() => {
 
             }}>
